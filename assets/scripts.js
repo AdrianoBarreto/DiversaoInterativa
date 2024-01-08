ac2323208024e83baf7e2522ce33918f8e8e9892
ADRIANO BARRETO <37568581+AdrianoBarreto@users.noreply.github.com>
2024-01-08 18:37:10 -0300

    Update scripts.js
    Ajustes Finos

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,14 +1,14 @@
 function playTicTacToe() {
-    // Abrir o jogo da velha em uma nova janela
+    // Abrir o Jogo da Velha em uma nova janela e justa a nova janela
     window.open('Jogodavelha.html', 'Jogo da Velha', 'width=500,height=500');
 }
 
 function openGuessingGame() {
-    // Abre uma nova janela para o jogo de adivinhação
+    // Abre uma nova janela para o Jogo de Adivinhação e justa a nova janela
     const gameWindow = window.open("Jogoadivinhacao.html", "_blank", "width=400,height=400");
 }
 
 function openJogodamemoria() {
-    // Abre uma nova janela para o jogo de adivinhação
+    // Abre uma nova janela para o Jogo da Memória e justa a nova janela
     const gameWindow = window.open("jogodamemoria.html", "_blank", "width=480,height=650");
 }
